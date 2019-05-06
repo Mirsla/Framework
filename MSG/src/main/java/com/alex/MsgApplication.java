7d14f0c8de4750f0260a564b681688c93f1100c5
Mirsla <844874989@qq.com>
2019-05-06 22:37:21 +0800

    rocketMQ 消息的简单测试Model

---
+++ MSG/src/main/java/com/alex/MsgApplication.java
@@ -0,0 +1,17 @@
+package com.alex;
+
+import com.alex.mq.annotation.EnableMQConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author: Alex
+ * @descripTion: 消息发送的简单实现
+ * @date: Created in  22:34 2019/5/6
+ * @modified By:
+ */
+@SpringBootApplication
+@EnableMQConfiguration
+public class MsgApplication {
+
+}
